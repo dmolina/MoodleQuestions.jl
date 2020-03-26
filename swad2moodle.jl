783c7a27c872ca17bd5508e71241630c710a5c23
Daniel Molina <dmolina@decsai.ugr.es>
2020-03-26 20:37:06 +0100

    Add swad2moodle program.

---
+++ swad2moodle.jl
@@ -0,0 +1,21 @@
+using QuestionsMoodle
+
+function main(ARGS)
+    if length(ARGS)!=2
+        println(stderr, "usage: textfile outputfile")
+        return
+    end
+
+    fname = ARGS[1]
+    foutput = ARGS[2]
+
+    if !isfile(fname)
+        println("Error, the file '$fname' does not exist")
+        return
+    end
+
+    quiz = read_swad(fname)
+    save_to_moodle(quiz, foutput)
+end
+
+isinteractive() || main(ARGS)
