d24c958493737532c2bd081320158ea500f1904b
Daniel Molina <dmolina@decsai.ugr.es>
2020-03-27 18:51:23 +0100

    Add missing file.

---
+++ src/MoodleQuestions.jl
@@ -0,0 +1,12 @@
+module MoodleQuestions
+
+include("read_write.jl")
+
+# Read from SWAD file
+export read_swad
+# Read from TXT file
+export read_txt
+# Save to Moodle
+export save_to_moodle
+
+end # module
