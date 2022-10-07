56102a68c4ddd166a1b67b3e85911123458393fe
Daniel Molina <danimolina@gmail.com>
2022-10-07 10:51:56 +0200

    Improve documentation.

--- src/serve.jl
+++ src/serve.jl
@@ -147,12 +147,15 @@ function handle(req::HTTP.Request)
 end
 
 """
-serve_quiz(port)
+serve_quiz(; ip=ntohing, port=8100)
 
 Run the text file in the txt format, and return the XML file.
 If there is only one category only one parameter is defined.
 
-serve_quiz(port)
+# Parameters:
+
+- ip: IP to restrict the IP, nothing implies all.
+- port: port to use.
 """
 function serve_quiz(; ip=nothing, port = 8100)
     fmessages = joinpath(dirname(pathof(MoodleQuestions)), "messages.ini")
